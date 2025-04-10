c9dd5dbcb212900511f035d08191fbd264c899e8
kycodee <ky.patton87@gmail.com>
2025-04-10 17:22:57 -0500

    Added background image to homepage

---
+++ marine-supply-frontend/app/homePhoto.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function HomePhoto(){
+  return (
+    <div className="bg-[url('./pexels-pixabay-164037.jpg')]" style={{height: '85vh', width: '100%', backgroundPosition: 'center',
+    backgroundSize: 'contain', backgroundRepeat: 'no-repeat'}}></div>
+  )
+}
+
+export default HomePhoto
