d2e84f13b6011525e0bcc15b60308aadf79bb0d0
kycodee <ky.patton87@gmail.com>
2025-04-10 15:56:36 -0500

    First navbar design rendered

--- marine-supply-frontend/app/page.tsx
+++ marine-supply-frontend/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import Navbar from "./navbar";
 
 export default function Home() {
   return (
-   <h1>Ky's Marine Supply</h1>
+    <>
+      <Navbar />
+    </>
   );
 }
