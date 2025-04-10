b7a85b358ebdde1f8d22d37de1102f6687c54eb4
kycodee <ky.patton87@gmail.com>
2025-04-10 17:22:57 -0500

    Added background image to homepage

--- marine-supply-frontend/app/page.tsx
+++ marine-supply-frontend/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Navbar from "./navbar";
+import HomePhoto from "./homePhoto";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <HomePhoto />
     </>
   );
 }
