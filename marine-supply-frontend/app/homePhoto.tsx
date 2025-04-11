4fa477f393aa7d2623a5d762d40ce0aafa95487f
kycodee <ky.patton87@gmail.com>
2025-04-11 12:13:03 -0500

    Added header to home page

--- marine-supply-frontend/app/homePhoto.tsx
+++ marine-supply-frontend/app/homePhoto.tsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 function HomePhoto(){
   return (
-    <div className="bg-[url('./pexels-pixabay-164037.jpg')]" style={{height: '85vh', width: '100%', backgroundPosition: 'center',
-    backgroundSize: 'contain', backgroundRepeat: 'no-repeat'}}></div>
+    <div className="bg-[url('./pexels-pixabay-164037.jpg')]" 
+    style={{
+        height: '85vh', 
+        width: '100%', 
+        backgroundPosition: 'center',
+        backgroundSize: 'contain', 
+        backgroundRepeat: 'no-repeat'}}>
+        <div className='flex flex-col justify-center items-center text-center' style={{height: '100%'}}>
+            <h1 className='text-5xl'>Louisiana's #1 Marine Equipment Distributor Since 2001</h1>
+            <p className='text-2xl mt-[20px] text-lime-500'>"Powering the Gulf Coast, One Vessel at a Time."</p>
+        </div>
+    </div>
   )
 }
 
