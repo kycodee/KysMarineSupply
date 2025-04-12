a5a0b43c7780c81528df924fcf0870cb65a5fc5c
kycodee <ky.patton87@gmail.com>
2025-04-12 17:44:44 -0500

    First carousel working(testing)

--- marine-supply-frontend/app/nav/engines/page.tsx
+++ marine-supply-frontend/app/nav/engines/page.tsx
@@ -1,9 +1,52 @@
 import React from 'react'
 
-function Engines(){
-  return (
-    <div className='h-[85vh]'>Enginess</div>
-  )
+function Engines() {
+    return (
+        <div className='h-[85vh] items-center flex justify-center'>
+            <div  >
+            
+                <div className="carousel rounded-box w-[85vw] ">
+                    <div className="carousel-item p-[10px]">
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+                            alt="Burger" />
+                    </div>
+                    <div className="carousel-item p-[10px]">
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
+                            alt="Burger" />
+                    </div>
+                    <div className="carousel-item p-[10px]">
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
+                            alt="Burger" />
+                    </div>
+                    <div className="carousel-item p-[10px]">
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
+                            alt="Burger" />
+                    </div>
+                    <div className="carousel-item p-[10px]">
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
+                            alt="Burger" />
+                    </div>
+                    <div className="carousel-item p-[10px]">
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
+                            alt="Burger" />
+                    </div>
+                    <div className="carousel-itemp-[10px]">
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
+                            alt="Burger" />
+                    </div>
+                </div>
+                
+                           
+            </div>
+        </div>
+    )
 }
 
 export default Engines
