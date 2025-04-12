341c5c7f48279357f7548e4790d23fbc1a1eb031
kycodee <ky.patton87@gmail.com>
2025-04-12 18:38:58 -0500

    Engine image and specifications added to card 1

--- marine-supply-frontend/app/nav/engines/page.tsx
+++ marine-supply-frontend/app/nav/engines/page.tsx
@@ -3,10 +3,89 @@ import React from 'react'
 function Engines() {
     return (
         <div className='h-[85vh] items-center flex justify-center'>
-            <div  >
-            
+            <div>
                 <div className="carousel rounded-box w-[85vw] ">
-                    <div className="carousel-item p-[10px]">
+                    <div className="carousel-item p-[10px] card bg-base-100 w-96 shadow-sm">
+                        <figure>
+                            <a href="https://www.labordeproducts.com/engines/mitsubishi-marine-engines/s6a3-y1mpta-3?hsLang=en"
+                            >
+                            <img
+                                src="https://www.labordeproducts.com/hubfs/s6a3.png"
+                                alt="S6A3-Y1MPTA-3" />
+                            </a>
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">S6A3-Y1MPTA-3</h2>
+                            <div className='flex justify-between'>
+                            <p>Power</p>
+                            <p>483 HP</p>
+                            </div>
+                            <div className='flex justify-between'>
+                            <p>Series</p>
+                            <p>A Series</p>
+                            </div>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item p-[10px] card bg-base-100 w-96 shadow-sm">
+                        <figure>
+                            <img
+                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                alt="Shoes" />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">Card Title</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item p-[10px] card bg-base-100 w-96 shadow-sm">
+                        <figure>
+                            <img
+                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                alt="Shoes" />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">Card Title</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item p-[10px] card bg-base-100 w-96 shadow-sm">
+                        <figure>
+                            <img
+                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                alt="Shoes" />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">Card Title</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item p-[10px] card bg-base-100 w-96 shadow-sm">
+                        <figure>
+                            <img
+                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                alt="Shoes" />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">Card Title</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <div className="carousel-item p-[10px]">
                         <img
                             src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
                             alt="Burger" />
@@ -40,10 +119,10 @@ function Engines() {
                         <img
                             src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
                             alt="Burger" />
-                    </div>
+                    </div> */}
                 </div>
-                
-                           
+
+
             </div>
         </div>
     )
