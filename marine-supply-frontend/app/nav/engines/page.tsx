05687f1ceb06d3233b6c2303e69478c7758c30ab
kycodee <ky.patton87@gmail.com>
2025-04-12 19:14:46 -0500

    Correct data and specifications inserted into all ENGINE cards

--- marine-supply-frontend/app/nav/engines/page.tsx
+++ marine-supply-frontend/app/nav/engines/page.tsx
@@ -6,23 +6,23 @@ function Engines() {
             <div>
                 <div className="carousel rounded-box w-[85vw] ">
                     <div className="carousel-item p-[10px] card bg-base-100 w-96 shadow-sm">
-                        <figure>
+                        <figure className='h-[305px]'>
                             <a href="https://www.labordeproducts.com/engines/mitsubishi-marine-engines/s6a3-y1mpta-3?hsLang=en"
                             >
-                            <img
-                                src="https://www.labordeproducts.com/hubfs/s6a3.png"
-                                alt="S6A3-Y1MPTA-3" />
+                                <img
+                                    src="https://www.labordeproducts.com/hubfs/s6a3.png"
+                                    alt="S6A3-Y1MPTA-3" />
                             </a>
                         </figure>
                         <div className="card-body">
                             <h2 className="card-title">S6A3-Y1MPTA-3</h2>
                             <div className='flex justify-between'>
-                            <p>Power</p>
-                            <p>483 HP</p>
+                                <p>Power</p>
+                                <p>483 HP</p>
                             </div>
                             <div className='flex justify-between'>
-                            <p>Series</p>
-                            <p>A Series</p>
+                                <p>Series</p>
+                                <p>A Series</p>
                             </div>
                             <div className="card-actions justify-end">
                                 <button className="btn btn-primary">Buy Now</button>
@@ -30,96 +30,125 @@ function Engines() {
                         </div>
                     </div>
                     <div className="carousel-item p-[10px] card bg-base-100 w-96 shadow-sm">
-                        <figure>
-                            <img
-                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                                alt="Shoes" />
-                        </figure>
+                        <figure className='h-[305px]'>
+                            <a href="https://www.labordeproducts.com/engines/mitsubishi-marine-engines/s12a2-y1mpta-3?hsLang=en"
+                            >
+                                <img
+                                    src="https://www.labordeproducts.com/hubfs/S12A2-1.png"
+                                    alt="S12A2-Y1MPTA-3" />
+                            </a>
+                        </figure >
                         <div className="card-body">
-                            <h2 className="card-title">Card Title</h2>
-                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                            <h2 className="card-title">S12A2-Y1MPTA-3</h2>
+                            <div className='flex justify-between'>
+                                <p>Power</p>
+                                <p>850 HP</p>
+                            </div>
+                            <div className='flex justify-between'>
+                                <p>Series</p>
+                                <p>A Series</p>
+                            </div>
                             <div className="card-actions justify-end">
                                 <button className="btn btn-primary">Buy Now</button>
                             </div>
                         </div>
                     </div>
                     <div className="carousel-item p-[10px] card bg-base-100 w-96 shadow-sm">
-                        <figure>
-                            <img
-                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                                alt="Shoes" />
+                        <figure className='h-[305px]'>
+                            <a href="https://www.labordeproducts.com/engines/mitsubishi-marine-engines/s6r-y1mptaw-3?hsLang=en"
+                            >
+                                <img
+                                    src="https://www.labordeproducts.com/hubfs/Imported%20images/S6R-Y1MPTA-3-768x624.png"
+                                    alt="S6R-Y1MPTAW-3" />
+                            </a>
                         </figure>
                         <div className="card-body">
-                            <h2 className="card-title">Card Title</h2>
-                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                            <h2 className="card-title">S6R-Y1MPTAW-3</h2>
+                            <div className='flex justify-between'>
+                                <p>Power</p>
+                                <p>590 HP</p>
+                            </div>
+                            <div className='flex justify-between'>
+                                <p>Series</p>
+                                <p>R Series</p>
+                            </div>
                             <div className="card-actions justify-end">
                                 <button className="btn btn-primary">Buy Now</button>
                             </div>
                         </div>
                     </div>
                     <div className="carousel-item p-[10px] card bg-base-100 w-96 shadow-sm">
-                        <figure>
-                            <img
-                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                                alt="Shoes" />
+                        <figure className='h-[305px]'>
+                            <a href="https://www.labordeproducts.com/engines/auxiliary/173-hp-bpu?hsLang=en"
+                            >
+                                <img
+                                    src="https://www.labordeproducts.com/hubfs/Imported%20images/LPI-173-1-768x762.png"
+                                    alt="173 HP BPU" />
+                            </a>
                         </figure>
                         <div className="card-body">
-                            <h2 className="card-title">Card Title</h2>
-                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                            <h2 className="card-title">173 HP BPU</h2>
+                            <div className='flex justify-between'>
+                                <p>Power</p>
+                                <p>173 HP</p>
+                            </div>
+                            <div className='flex justify-between'>
+                                <p>Series</p>
+                                <p>BPU</p>
+                            </div>
                             <div className="card-actions justify-end">
                                 <button className="btn btn-primary">Buy Now</button>
                             </div>
                         </div>
                     </div>
                     <div className="carousel-item p-[10px] card bg-base-100 w-96 shadow-sm">
-                        <figure>
-                            <img
-                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                                alt="Shoes" />
+                        <figure className='h-[305px]'>
+                            <a href="https://www.labordeproducts.com/engines/auxiliary/429-hp-low-profile-bpu?hsLang=en"
+                            >
+                                <img
+                                    src="https://www.labordeproducts.com/hubfs/Imported%20images/S6B3-429BPU-1-768x605.png"
+                                    alt="429 HP Low Profile BPU" />
+                            </a>
                         </figure>
                         <div className="card-body">
-                            <h2 className="card-title">Card Title</h2>
-                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                            <h2 className="card-title">429 HP Low Profile BPU</h2>
+                            <div className='flex justify-between'>
+                                <p>Power</p>
+                                <p>429 HP</p>
+                            </div>
+                            <div className='flex justify-between'>
+                                <p>Series</p>
+                                <p>BPU</p>
+                            </div>
                             <div className="card-actions justify-end">
                                 <button className="btn btn-primary">Buy Now</button>
                             </div>
                         </div>
                     </div>
-                    {/* <div className="carousel-item p-[10px]">
-                        <img
-                            src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-                            alt="Burger" />
+                    <div className="carousel-item p-[10px] card bg-base-100 w-96 shadow-sm">
+                        <figure className='h-[305px]'>
+                            <a href="https://www.labordeproducts.com/engines/auxiliary/429-hp-bpu?hsLang=en"
+                            >
+                                <img
+                                    src="https://www.labordeproducts.com/hubfs/Imported%20images/DSC0022-768x623.png"
+                                    alt="429 HP BPU" />
+                            </a>
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">429 HP BPU</h2>
+                            <div className='flex justify-between'>
+                                <p>Power</p>
+                                <p>429 HP</p>
+                            </div>
+                            <div className='flex justify-between'>
+                                <p>Series</p>
+                                <p>BPU</p>
+                            </div>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
                     </div>
-                    <div className="carousel-item p-[10px]">
-                        <img
-                            src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
-                            alt="Burger" />
-                    </div>
-                    <div className="carousel-item p-[10px]">
-                        <img
-                            src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
-                            alt="Burger" />
-                    </div>
-                    <div className="carousel-item p-[10px]">
-                        <img
-                            src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
-                            alt="Burger" />
-                    </div>
-                    <div className="carousel-item p-[10px]">
-                        <img
-                            src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
-                            alt="Burger" />
-                    </div>
-                    <div className="carousel-item p-[10px]">
-                        <img
-                            src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
-                            alt="Burger" />
-                    </div>
-                    <div className="carousel-itemp-[10px]">
-                        <img
-                            src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
-                            alt="Burger" />
-                    </div> */}
                 </div>
 
 
