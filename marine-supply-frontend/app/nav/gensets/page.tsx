3432fce4acae95ba38351dd6367c4eb0d30dbaa9
kycodee <ky.patton87@gmail.com>
2025-04-13 18:00:56 -0500

    Correct data and specifications inserted into all GENSET cards

--- marine-supply-frontend/app/nav/gensets/page.tsx
+++ marine-supply-frontend/app/nav/gensets/page.tsx
@@ -1,9 +1,159 @@
 import React from 'react'
 
-function Gensets(){
-  return (
-    <div className='h-[85vh]'>Gensets</div>
-  )
+function Gensets() {
+    return (
+        <div className='h-[85vh] items-center flex justify-center'>
+            <div>
+                <div className="carousel rounded-box w-[85vw] ">
+                    <div className="carousel-item p-[10px] card bg-base-100 w-96 shadow-sm">
+                        <figure className='h-[305px]'>
+                            <a href="https://www.labordeproducts.com/gensets/lpi-y-40?hsLang=en"
+                            >
+                                <img
+                                    src="https://www.labordeproducts.com/hubfs/LPI-Y-40.png"
+                                    alt="LPI-Y-40 Generator" />
+                            </a>
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">LPI-Y-40 Generator</h2>
+                            <div className='flex justify-between'>
+                                <p>Power</p>
+                                <p>40kW</p>
+                            </div>
+                            <div className='flex justify-between'>
+                                <p>Manufacturer</p>
+                                <p>Yanmar</p>
+                            </div>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item p-[10px] card bg-base-100 w-96 shadow-sm">
+                        <figure className='h-[305px]'>
+                            <a href="https://www.labordeproducts.com/gensets/lpi-f-60?hsLang=en"
+                            >
+                                <img
+                                    src="https://www.labordeproducts.com/hubfs/LPI-F-60.png"
+                                    alt="LPI-F-60 Generator" />
+                            </a>
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">LPI-F-60 Generator</h2>
+                            <div className='flex justify-between'>
+                                <p>Power</p>
+                                <p>60kW</p>
+                            </div>
+                            <div className='flex justify-between'>
+                                <p>Manufacturer</p>
+                                <p>FPT</p>
+                            </div>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item p-[10px] card bg-base-100 w-96 shadow-sm">
+                        <figure className='h-[305px]'>
+                            <a href="https://www.labordeproducts.com/gensets/lpi-f-99?hsLang=en"
+                            >
+                                <img
+                                    src="https://www.labordeproducts.com/hubfs/LPI-F-99.png"
+                                    alt="LPI-F-99 Generator" />
+                            </a>
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">LPI-F-99 Generator</h2>
+                            <div className='flex justify-between'>
+                                <p>Power</p>
+                                <p>99kW</p>
+                            </div>
+                            <div className='flex justify-between'>
+                                <p>Manufacturer</p>
+                                <p>FPT</p>
+                            </div>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item p-[10px] card bg-base-100 w-96 shadow-sm">
+                        <figure className='h-[305px]'>
+                            <a href="https://www.labordeproducts.com/gensets/lpi-y-40kc?hsLang=en"
+                            >
+                                <img
+                                    src="https://www.labordeproducts.com/hubfs/LPI-Y-40.png"
+                                    alt="LPI-Y-40KC Generator" />
+                            </a>
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">LPI-Y-40KC Generator</h2>
+                            <div className='flex justify-between'>
+                                <p>Power</p>
+                                <p>40kW Keel Cooled</p>
+                            </div>
+                            <div className='flex justify-between'>
+                                <p>Manufacturer</p>
+                                <p>Yanmar</p>
+                            </div>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item p-[10px] card bg-base-100 w-96 shadow-sm">
+                        <figure className='h-[305px]'>
+                            <a href="https://www.labordeproducts.com/gensets/lpi-f-65kc?hsLang=en"
+                            >
+                                <img
+                                    src="https://www.labordeproducts.com/hubfs/LPI-F-60.png"
+                                    alt="LPI-F-65KC Generator" />
+                            </a>
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">LPI-F-65KC Generator</h2>
+                            <div className='flex justify-between'>
+                                <p>Power</p>
+                                <p>65kW Keel Cooled</p>
+                            </div>
+                            <div className='flex justify-between'>
+                                <p>Manufacturer</p>
+                                <p>FPT</p>
+                            </div>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item p-[10px] card bg-base-100 w-96 shadow-sm">
+                        <figure className='h-[305px]'>
+                            <a href="https://www.labordeproducts.com/gensets/lpi-f45-99kc?hsLang=en"
+                            >
+                                <img
+                                    src="https://www.labordeproducts.com/hubfs/LPI-F45-99KC.png"
+                                    alt="LPI-F45-99KC Generator" />
+                            </a>
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">LPI-F45-99KC Generator</h2>
+                            <div className='flex justify-between'>
+                                <p>Power</p>
+                                <p>99kW Keel Cooled</p>
+                            </div>
+                            <div className='flex justify-between'>
+                                <p>Manufacturer</p>
+                                <p>FPT</p>
+                            </div>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    </div>
+            </div>
+        </div>
+    )
 }
 
 export default Gensets
