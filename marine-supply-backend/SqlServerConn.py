8aec9791f03bee5ba5738c55e4fbd417ebc79ec8
kycodee <ky.patton87@gmail.com>
2025-04-15 21:43:56 -0500

    Successfully set up local SQL Server with Docker and verified connection

---
+++ marine-supply-backend/SqlServerConn.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+
+connection_url = URL.create(
+    "mssql+pyodbc",
+    username="SA",
+    password="MarineSupply@2025",
+    host="localhost",
+    port=1433,
+    database="master",  
+    query={
+        "driver": "ODBC Driver 18 for SQL Server",
+        "Encrypt": "yes",
+        "TrustServerCertificate": "yes",
+    },
+)
+
+engine = create_engine(connection_url)
+
+try:
+    with engine.connect() as connection:
+        result = connection.execute("SELECT @@VERSION")
+        for row in result:
+            print("Connection successful! SQL Server version:")
+            print(row[0])
+except Exception as e:
+    print("Connection failed:")
+    print(e)
