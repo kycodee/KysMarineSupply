673c16f17c0abae0abb6fa31fa541ba618cb6747
kycodee <ky.patton87@gmail.com>
2025-04-18 08:26:14 -0500

    Created Order schema

--- marine-supply-backend/schemas.py
+++ marine-supply-backend/schemas.py
@@ -1,17 +1,39 @@
 from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
 
+
+# Engine Schema
 class EngineBase(BaseModel):
     name: str
     power: str
     series: str
     image_url: str
     link: str
+    current_stock: int
 
 class EngineCreate(EngineBase):
     pass
 
 class EngineRead(EngineBase):
     id: int
+
+    class Config:
+        from_attributes = True
+
+
+# Order Schema
+class OrderBase(BaseModel):
+    engine_id: int
+    quantity: int
+
+class OrderCreate(OrderBase):
+    pass
+
+class OrderRead(OrderBase):
+    id: int
+    timestamp: datetime
+    engine: Optional[EngineRead]
 
     class Config:
         from_attributes = True
