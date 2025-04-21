a97b7eaf32a12d583df35566eb273778fff1d91e
kycodee <ky.patton87@gmail.com>
2025-04-21 02:32:54 -0500

    Updated Navbar to show only engines and about us page

--- marine-supply-frontend/app/navbar.tsx
+++ marine-supply-frontend/app/navbar.tsx
@@ -12,18 +12,12 @@ function Navbar() {
             <div className="navbar-center">
                 <ul className="menu menu-horizontal px-1 text-lg text-lime-500">
                     <li><Link href={'/nav/engines'}>Engines</Link></li>
-                    <li><Link href={'/nav/gensets'}>Gensets</Link></li>
                     <li><Link href={'/nav/about-page'}>About Us</Link></li>
                 </ul>
             </div>
             <div className="navbar-end">
                 <div className="flex-none">
-                    <button className="btn btn-ghost btn-circle">
-                        <div className="indicator">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> </svg>
-                            <span className="badge badge-xs badge-primary indicator-item"></span>
-                        </div>
-                    </button>
+                   <h1 className='mr-[60px]'>Owned By: <span className='underline'>Kylan Patton</span> </h1>
                 </div>
             </div>
         </div>
